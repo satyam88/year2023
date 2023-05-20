38fa0b671a11eead52d97683fbeff1a3cc7380a1
“Satyam <satyam@gmail.com>
2023-05-20 11:31:47 +0530

    hello

---
+++ src/main/java/com/example/year2023/feb.java
@@ -0,0 +1,10 @@
+package com.example.year2023;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class feb {
+    @GetMapping("/myfeb")
+    public String getData() {return  "Jan has 3 long weekend"; }
+}
